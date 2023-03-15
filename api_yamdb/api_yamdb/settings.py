1135d6f7a75f0d57e3e8ff85fbfed66ea3fe7508
Sergey Kozlov <serand.kozlov@gmail.com>
2023-03-15 11:22:02 +0300

    Static issue

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", default="xxxxx")
 
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").upper() == "TRUE"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", default="*")
 
